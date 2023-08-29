e269d3b0c5874bef0ac0dda17177a8e76bd67d87
b4thefomo <39229181+b4thefomo@users.noreply.github.com>
2023-08-29 20:50:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -141,12 +141,12 @@ tools = [
 
 system_message = SystemMessage(
     content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
-            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
+            you do not make things up, you will try as hard as possible to gather facts & data to back up the research. You particularly specialize in gathering interesting facts about people that can be used for cold email outreach to get them interested in responding.
             
             Please make sure you complete the objective above with the following rules:
             1/ You should do enough research to gather as much information as possible about the objective
             2/ If there are url of relevant links & articles, you will scrape it to gather more information
-            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
+            3/ After scraping & search, you should think "is there any new or interesting things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
             4/ You should not make things up, you should only write facts & data that you have gathered
             5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
             6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
